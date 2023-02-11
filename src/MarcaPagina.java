a38851a9dcbf20081bf47492851573d67fed88da
Bianca Milea <alumil2013@ieselcaminas.org>
2023-02-11 17:48:18 +0100

    Marca Pagina Ioana Bianca Milea

--- src/MarcaPagina.java
+++ src/MarcaPagina.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class MarcaPagina {
+public class MarcaPagina {
+    int numPagina;
+    String infoUltPagina;
+
+    public MarcaPagina (){
+        this.numPagina = 0;
+        this.infoUltPagina = "";
+    }
+
+    public void incrementarNumPagina (){
+        numPagina++;
+    }
+
+    public String obtenerInfoUltPagina(){
+        return infoUltPagina;
+    }
+
+    public void comenzarInicio(){
+        numPagina = 0;
+    }
+
 }
